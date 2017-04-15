b08c2cfadf626deb16f5d492a48ff5b32d008710
bumpinou <bumpmann@gmail.com>
2017-04-15 14:45:21 +0200

    Fix compilation

---
+++ mkdist.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# this needs `npm install uglify-js browserify -g`
+
+mkdir -p ./dist/js
+mkdir -p ./dist/node_modules/materialize-css/dist/css
+mkdir -p ./dist/node_modules/materialize-css/dist/js
+mkdir -p ./dist/node_modules/materialize-css/dist/fonts/roboto
+mkdir -p ./dist/node_modules/angular
+
+cp -R css img partials favicon.png index.html ./dist/
+cp ./node_modules/materialize-css/dist/css/materialize.css ./dist/node_modules/materialize-css/dist/css/
+cp ./node_modules/materialize-css/dist/js/materialize.min.js ./dist/node_modules/materialize-css/dist/js/
+cp ./node_modules/materialize-css/dist/fonts/roboto/Roboto-*.woff2 ./dist/node_modules/materialize-css/dist/fonts/roboto/
+cp ./node_modules/angular/angular.min.js ./dist/node_modules/angular/
+browserify ./js/app.js -o ./dist/js/app.js
+uglifyjs ./dist/js/app.js --compress --screw-ie8 -o ./dist/js/app.js
